31618081be4eeccbb0f6ae244e3ae0167bdd01df
ganeshkumar92 <ganesh_kumar_21@hotmail.com>
2025-08-29 09:27:47 +0400

    B: added classes for circle representations and can draw player on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player
 
 from constants import *
 
@@ -8,12 +9,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
+        player1.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS
 if __name__ == "__main__":
     main()
